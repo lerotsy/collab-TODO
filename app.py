720d7ab4a2c673fa16d7ef7bf945beb5b3042cc9
lerotsy <rotsy@pm.me>
2024-01-14 22:25:06 -0500

    add endpoints

--- app.py
+++ app.py
@@ -1,10 +1,32 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from models.todo_list import ToDoList
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo-app.db'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
 
+@app.route('/todolists', methods=['POST'])
+def create_todolist():
+    data = request.json
+    new_list = ToDoList(title=data['title'])
+    db.session.add(new_list)
+    db.session.commit()
+    return jsonify({'id': new_list.id}), 201
+
+@app.route('/todolists', methods=['GET'])
+def get_todolists():
+    lists = ToDoList.query.all()
+    return jsonify([{'id': lst.id, 'title': lst.title} for lst in lists]), 200
+
+
 if __name__ == '__main__':
     app.run(debug=True)
+
--- models/todo_list.py
+++ models/todo_list.py
@@ -4,6 +4,11 @@ class ToDoList(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False, 
+                           default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, 
+                           default=db.func.current_timestamp(),
+                           onupdate=db.func.current_timestamp())
     tasks = db.relationship('Task', backref='todolist', lazy=True)
 
     def __repr__(self):
