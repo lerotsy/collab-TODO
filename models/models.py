9d0d72aa1b98c75bd0aed6926fc828448d0d8771
lerotsy <rotsy@pm.me>
2024-01-16 12:13:37 -0500

    Add shared todo list functionality

--- models/models.py
+++ models/models.py
@@ -9,13 +9,29 @@ class TaskStatus(enum.Enum):
     COMPLETED = 'completed'
 
 
+class Permission(enum.Enum):
+    READ = 'read'
+    WRITE = 'write'
+    ADMIN = 'admin'
+
+
 db = SQLAlchemy()
+
+shared_lists = db.Table('shared_lists',
+    db.Column('user_id', db.Integer, db.ForeignKey('user.id'), primary_key=True),
+    db.Column('list_id', db.Integer, db.ForeignKey('to_do_list.id'), primary_key=True),
+    db.Column('permissions', db.Enum(Permission), default=Permission.READ)
+)
+
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True, nullable=False)
     password_hash = db.Column(db.String(128))
     lists = db.relationship('ToDoList', backref='user', lazy=True)
+    shared_lists = db.relationship('ToDoList', secondary=shared_lists, 
+                                   lazy='subquery', 
+                                   backref=db.backref('shared_with', lazy=True))
 
     def __repr__(self):
         return f'<User {self.username}>'
