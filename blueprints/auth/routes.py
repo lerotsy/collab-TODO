8b75e982ce70a7684a16245cc854e99d19706447
lerotsy <rotsy@pm.me>
2024-01-18 11:06:38 -0500

    move user related endpoint to auth blueprit

--- blueprints/auth/routes.py
+++ blueprints/auth/routes.py
@@ -1,7 +1,9 @@
-from models.models import  User
-from flask import request, jsonify
-from werkzeug.security import check_password_hash
-from flask_jwt_extended import  create_access_token
+from models.models import db, User
+from flask import request, jsonify, abort
+from flask_migrate import Migrate
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import jwt_required, create_access_token
+from datetime import datetime
 from . import auth_blueprint
 
 
@@ -15,3 +17,55 @@ def login():
         access_token = create_access_token(identity=username)
         return jsonify(access_token=access_token)
     return jsonify({"msg": "Bad username or password"}), 401
+
+
+@auth_blueprint.route('/users', methods=['POST'])
+def create_user():
+    data = request.json
+    if not data or 'username' not in data or 'password' not in data:
+        abort(400, 'Username and password are required.')
+
+    # Check if user already exists
+    if User.query.filter_by(username=data['username']).first():
+        abort(400, 'Username already exists.')
+
+    hashed_password = generate_password_hash(data['password'])
+    new_user = User(username=data['username'], password_hash=hashed_password)
+    db.session.add(new_user)
+    db.session.commit()
+    return jsonify({'id': new_user.id, 'username': new_user.username}), 201
+
+
+@jwt_required()
+@auth_blueprint.route('/users', methods=['GET'])
+def get_users():
+    users = User.query.all()
+    return jsonify([{'id': user.id, 'username': user.username} for user in users]), 200
+
+
+@jwt_required()
+@auth_blueprint.route('/users/<int:user_id>', methods=['GET'])
+def get_user(user_id):
+    user = User.query.get_or_404(user_id)
+    return jsonify({'id': user.id, 'username': user.username}), 200
+
+
+@jwt_required()
+@auth_blueprint.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    user = User.query.get_or_404(user_id)
+    data = request.json
+    user.username = data.get('username', user.username)
+    if 'password' in data:
+        user.password_hash = generate_password_hash(data['password'])
+    db.session.commit()
+    return jsonify({'id': user.id, 'username': user.username}), 200
+
+
+@jwt_required()
+@auth_blueprint.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    user = User.query.get_or_404(user_id)
+    db.session.delete(user)
+    db.session.commit()
+    return jsonify({'message': 'User deleted'}), 200
--- blueprints/todo/routes.py
+++ blueprints/todo/routes.py
@@ -56,53 +56,6 @@ def delete_todo_list(list_id):
     return jsonify({'message': 'Todo list deleted'}), 200
 
 
-@todo_blueprint.route('/users', methods=['POST'])
-def create_user():
-    data = request.json
-    if not data or 'username' not in data or 'password' not in data:
-        abort(400, 'Username and password are required.')
-
-    # Check if user already exists
-    if User.query.filter_by(username=data['username']).first():
-        abort(400, 'Username already exists.')
-
-    hashed_password = generate_password_hash(data['password'])
-    new_user = User(username=data['username'], password_hash=hashed_password)
-    db.session.add(new_user)
-    db.session.commit()
-    return jsonify({'id': new_user.id, 'username': new_user.username}), 201
-
-
-@todo_blueprint.route('/users', methods=['GET'])
-def get_users():
-    users = User.query.all()
-    return jsonify([{'id': user.id, 'username': user.username} for user in users]), 200
-
-
-@todo_blueprint.route('/users/<int:user_id>', methods=['GET'])
-def get_user(user_id):
-    user = User.query.get_or_404(user_id)
-    return jsonify({'id': user.id, 'username': user.username}), 200
-
-
-@todo_blueprint.route('/users/<int:user_id>', methods=['PUT'])
-def update_user(user_id):
-    user = User.query.get_or_404(user_id)
-    data = request.json
-    user.username = data.get('username', user.username)
-    if 'password' in data:
-        user.password_hash = generate_password_hash(data['password'])
-    db.session.commit()
-    return jsonify({'id': user.id, 'username': user.username}), 200
-
-
-@todo_blueprint.route('/users/<int:user_id>', methods=['DELETE'])
-def delete_user(user_id):
-    user = User.query.get_or_404(user_id)
-    db.session.delete(user)
-    db.session.commit()
-    return jsonify({'message': 'User deleted'}), 200
-
 
 @todo_blueprint.route('/tasks', methods=['POST'])
 @jwt_required()
