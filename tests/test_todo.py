199176add8ed2559e0e2682afa2b3eb084b40bb7
lerotsy <rotsyrazafy@gmail.com>
2024-01-24 10:01:48 -0500

    add test for task

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -1,13 +1,13 @@
 import unittest
 from app import create_app, db
-from models.models import User, ToDoList, Task
+from models.models import User, ToDoList, Task, TaskStatus
 from werkzeug.security import generate_password_hash
 from flask_jwt_extended import create_access_token
 
 class ToDoTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.app = create_app('testing')
+        self.app = create_app('test')
         self.app_context = self.app.app_context()
         self.app_context.push()
         self.client = self.app.test_client()
@@ -19,7 +19,8 @@ class ToDoTestCase(unittest.TestCase):
         db.session.commit()
 
         self.test_user_id = user.id
-        self.test_todo_list = ToDoList(title='Test ToDo List', user_id=self.test_user_id)
+        self.test_todo_list = ToDoList(
+            title='Test ToDo List', user_id=self.test_user_id)
         db.session.add(self.test_todo_list)
         db.session.commit()
 
@@ -43,7 +44,7 @@ class ToDoTestCase(unittest.TestCase):
         self.assertIn('Test ToDo List', response.get_json()['title'])
 
     def test_update_todolist(self):
-        response = self.client.put(f'/todolists/{self.test_todo_list.id}', 
+        response = self.client.put(f'/todolists/{self.test_todo_list.id}',
                                    json={'title': 'Updated ToDo List'},
                                    headers={'Authorization': f'Bearer {self.token}'})
         self.assertEqual(response.status_code, 200)
@@ -53,3 +54,18 @@ class ToDoTestCase(unittest.TestCase):
         response = self.client.delete(f'/todolists/{self.test_todo_list.id}',
                                       headers={'Authorization': f'Bearer {self.token}'})
         self.assertEqual(response.status_code, 200)
+
+    def test_create_task(self):
+        response = self.client.post('/tasks',
+                                    json={
+                                        'description': 'New Task',
+                                        'status': 'NEW',
+                                        'due_date': '2023-01-01T00:00:00',
+                                        'list_id': self.test_todo_list.id
+                                    },
+                                    headers={'Authorization': f'Bearer {self.token}'})
+        self.assertEqual(response.status_code, 201)
+        data = response.get_json()
+        self.assertIn('New Task', data['description'])
+
+        self.test_task_id = data['id']
